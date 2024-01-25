6f235978d85a99afea9291396571770a47f8ca08
Petri  Räsänen <petri.rasanen@schibsted.com>
2024-01-25 16:38:45 +0200

    25jan24_1638

--- client/src/data/readyUnits.ts
+++ client/src/data/readyUnits.ts
@@ -17,6 +17,23 @@ export interface ReadyUnit {
 // Desert Rats
 export const desertRatsUnits: ReadyUnit[] = [
     {
+        name: 'Sherman HQ',
+        teams: [
+            {
+                name: 'Sherman M4A1 HQ',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: 'Sherman M4A1 HQ',
+                exp: 'veteran',
+                points: 0
+            }
+        ],
+        points: 0,
+        desc: 'A HQ unit with two Shermans.'
+    },
+    {
         name: 'Tank HQ',
         teams: [
             {
@@ -31,7 +48,7 @@ export const desertRatsUnits: ReadyUnit[] = [
             }
         ],
         points: 0,
-        desc: 'A HQ unit with tank'
+        desc: 'A HQ unit with tanks. Containing Sherman and Grant.'
     },
     {
         name: 'Rifle HQ',
@@ -51,7 +68,7 @@ export const desertRatsUnits: ReadyUnit[] = [
         desc: 'A HQ unit with tank'
     },
     {
-        name: 'Rifle platoon',
+        name: 'Rifle platoon (7 teams)',
         teams: [
             {
                 name: 'Bren & SMLE team',
@@ -79,18 +96,266 @@ export const desertRatsUnits: ReadyUnit[] = [
                 points: 0
             },
             {
-                name: 'Boys AT-rifle', // need to do to database pg 177
+                name: 'Boys AT-rifle team',
                 exp: 'veteran',
                 points: 0
             },
             {
-                name: '2-inch mortar', // need to do to database pg 177
+                name: '2-inch mortar team',
                 exp: 'veteran',
                 points: 0
             }
         ],
         points: 0,
-        desc: 'A good solid infantry unit, with Boys at-rifle and 2-inch mortar'
+        desc: 'A good solid infantry unit, with Boys at-rifle and 2-inch mortar. Very good in close combat.'
+    },
+    {
+        name: 'Rifle platoon (9 teams)',
+        teams: [
+            {
+                name: 'Bren & SMLE team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: 'Bren & SMLE team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: 'Bren & SMLE team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: 'Bren & SMLE team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: 'Bren & SMLE team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: 'Bren & SMLE team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: 'Bren & SMLE team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: 'Boys AT-rifle team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: '2-inch mortar team',
+                exp: 'veteran',
+                points: 0
+            }
+        ],
+        points: 0,
+        desc: 'A good solid infantry unit, with Boys at-rifle and 2-inch mortar. Very good in close combat.'
+    },
+    {
+        name: '3-inch mortar platoon (3 pipes)',
+        teams: [
+            {
+                name: '3-inch mortar team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: '3-inch mortar team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: '3-inch mortar team',
+                exp: 'veteran',
+                points: 0
+            },
+        ],
+        points: 0,
+        desc: 'A good infantry support, that can fire smoke bombardment too'
+    },
+    {
+        name: '3-inch mortar platoon (4 pipes)',
+        teams: [
+            {
+                name: '3-inch mortar team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: '3-inch mortar team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: '3-inch mortar team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: '3-inch mortar team',
+                exp: 'veteran',
+                points: 0
+            },
+        ],
+        points: 0,
+        desc: 'A good infantry support, that can fire smoke bombardment too'
+    },
+    {
+        name: '3-inch mortar platoon (6 pipes)',
+        teams: [
+            {
+                name: '3-inch mortar team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: '3-inch mortar team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: '3-inch mortar team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: '3-inch mortar team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: '3-inch mortar team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: '3-inch mortar team',
+                exp: 'veteran',
+                points: 0
+            }
+        ],
+        points: 0,
+        desc: 'A good infantry support, that can fire smoke bombardment too'
+    },
+    {
+        name: '6 pdr anti-tank platoon (2 pipes)',
+        teams: [
+            {
+                name: '6 pdr anti-tank team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: '6 pdr anti-tank team',
+                exp: 'veteran',
+                points: 0
+            }
+        ],
+        points: 0,
+        desc: 'A good anti-tank gun. Anything else than tiger does not want to take this to frontal armour.'
+    },
+    {
+        name: '6 pdr anti-tank platoon (3 pipes)',
+        teams: [
+            {
+                name: '6 pdr anti-tank team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: '6 pdr anti-tank team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: '6 pdr anti-tank team',
+                exp: 'veteran',
+                points: 0
+            }
+        ],
+        points: 0,
+        desc: 'A good anti-tank gun. Anything else than tiger does not want to take this to frontal armour.'
+    },
+    {
+        name: '6 pdr anti-tank platoon (4 pipes)',
+        teams: [
+            {
+                name: '6 pdr anti-tank team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: '6 pdr anti-tank team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: '6 pdr anti-tank team',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: '6 pdr anti-tank team',
+                exp: 'veteran',
+                points: 0
+            }
+        ],
+        points: 0,
+        desc: 'A good anti-tank gun. Anything else than tiger does not want to take this to frontal armour.'
+    },    
+    {
+        name: 'Sherman Armoured Troop (3 shermans)',
+        teams: [
+            {
+                name: 'Sherman M4A1',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: 'Sherman M4A1',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: 'Sherman M4A1',
+                exp: 'veteran',
+                points: 0
+            }
+        ],
+        points: 0,
+        desc: 'A powerful armoured sherman unit.'
+    },    
+    {
+        name: 'Sherman Armoured Troop (2 shermans, 1 grant)',
+        teams: [
+            {
+                name: 'Sherman M4A1',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: 'Sherman M4A1',
+                exp: 'veteran',
+                points: 0
+            },
+            {
+                name: 'Grant',
+                exp: 'veteran',
+                points: 0
+            }
+        ],
+        points: 0,
+        desc: 'Crush your enemies with 2 shermans and 1 grant.'
     }
 ];
 
